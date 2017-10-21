2e769b0a08bb29967a8b6f58d65b99d874d7e04a
Yonlada Prasert <60070077@kmitl.ac.th>
2017-10-21 14:31:49 +0700

    Add yaimai

--- print.py
+++ print.py
@@ -13,5 +13,11 @@ def print_it(times):
         print("KUY")
         print("KUY")
         print("KUY")
+        print("Yaimai so cute")
+        print("Yaimai so cute")
+        print("Yaimai so cute")
+        print("Yaimai so cute")
+        print("Yaimai so cute")
+        print("Yaimai so cute")
 
 print_it(int(input())
